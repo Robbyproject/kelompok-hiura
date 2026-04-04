e0548afb15a6d0c6a0da71fc078394fbf9470e13
Suharis Baihaqi Nur <prdrap776@gmail.com>
2026-04-05 01:10:20 +0700

    mismatch variable name fix, TransaksiPOS.cetak_struk() now printing not returning string, implemented class method in main file.

--- 25110300032_25110300018_25110300029.py
+++ 25110300032_25110300018_25110300029.py
@@ -1,32 +1,26 @@
-pass # :)
+from modules.KomponenHarga import KomponenHarga
+from modules.PajakPPN import PajakPPN
+from modules.TransaksiPOS import TransaksiPOS
 
-def inputPesanan():
-    list_pesanan = []
+def inputPesanan(transaksiPOS: TransaksiPOS):
     while True:
         print("\n--- Input Pesanan ---")
         nama_makanan = input("Nama Makanan (atau ketik 'selesai'): ")
         if nama_makanan == "selesai":
-            return list_pesanan
+            return
         harga_satuan = int(input(f"Harga Satuan {nama_makanan}: "))
         jumlah = int(input(f"Jumlah {nama_makanan}: "))
+        transaksiPOS.tambah_item(nama_makanan, harga_satuan, jumlah)
 
-        list_pesanan.append({
-            "nama_makanan": nama_makanan,
-            "harga_satuan": harga_satuan,
-            "jumlah"      : jumlah
-        })
+def bayarTagihan(transaksiPOS: TransaksiPOS):
+    while True:
+        uang_bayar = int(input("Masukkan Uang Bayar: Rp"))
+        if transaksiPOS.proses_pembayaran(uang_bayar):
+            print(f"BERHASIL: Kembalian Anda adalah Rp{uang_bayar - transaksiPOS.total_akhir :,}\n")
+            return
+        
+        print(f"GAGAL: Uang Kurang! (Kurang Rp{transaksiPOS.total_akhir - uang_bayar :,})\n")
 
-def printStrukPembayaran(nama_pelanggan, nomor_meja, list_pesanan):
-    print("\n" + "=" * 40)
-    print(f"       STRUK PEMBAYARAN - MEJA {nomor_meja}")
-    print("=" * 40)
-    print(f"Pelanggan: {nama_pelanggan}")
-    print("-" * 40)
-    for pesanan in list_pesanan:
-        print(f"{pesanan['nama_makanan']:<20}x{pesanan['jumlah']:>2}  Rp{pesanan['jumlah'] * pesanan['harga_satuan'] :>11,}")
-    print("-" * 40)
-    # logic from classes
-    print("=" * 40)
 
 def main():
     print("=" * 40)
@@ -34,11 +28,10 @@ def main():
     print("=" * 40)
     nama_pelanggan = input("Nama Pelanggan : ")
     nomor_meja     = input("Nomor Meja     : ")
+    transaksi_pos = TransaksiPOS(nama_pelanggan, nomor_meja)
 
-    pesanan = inputPesanan()
-    printStrukPembayaran(nama_pelanggan, nomor_meja, pesanan)
-
-
-
+    inputPesanan(transaksi_pos)
+    transaksi_pos.cetak_struk()
+    bayarTagihan(transaksi_pos)
 
 main()
