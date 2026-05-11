8109addc335baab6f134e84da79ca85fa6038cee
Robbyproject <ismailmarjuki8569@gmail.com>
2026-05-11 21:44:34 +0700

    Update Function Urutan_koleksi dan Validasi id

--- UAS/modules/ManajerPerpustakaan.py
+++ UAS/modules/ManajerPerpustakaan.py
@@ -6,8 +6,11 @@ class ManajerPerpustakaan:
         self.koleksi = []
         self.terurut = False
 
-    def validasi_id_unik(self, id_baru): # return bool
-        pass
+    def validasi_id_unik(self, id_baru): # return bool,,, Robby
+        for buku in self.koleksi:
+            if buku.id == id_baru:
+                return False
+        return True
 
     def tambah_buku_baru(self): # void
         pass
@@ -15,8 +18,17 @@ class ManajerPerpustakaan:
     def tampilkan_koleksi(self): # void
         pass
 
-    def urutkan_koleksi(self): # return void, bubble sort
-        pass
+    def urutkan_koleksi(self): # return void, bubble sort,,,, Robby
+        if self.terurut:
+            print("Koleksi sudah terurut.")
+            return
+        n = len(self.koleksi)
+        for i in range(n):
+            for j in range(0, n-i-1):
+                if self.koleksi[j].id > self.koleksi[j+1].id:
+                    self.koleksi[j], self.koleksi[j+1] = self.koleksi[j+1], self.koleksi[j]
+        self.terurut = True
+        print("Koleksi berhasil diurutkan berdasarkan ID.")
 
     def cari_buku(self, id_target): # return object dari class Buku / None, binary search
-        pass
+        return 
